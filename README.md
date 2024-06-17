35dac9880420f1aa880cb7bac50a667fc2ced410
Rekt Party <zhitaburnurli@gmail.com>
2024-06-17 14:53:02 +0700

    Add update.sh

---
+++ README.md
@@ -0,0 +1,12 @@
+# Legado booksource collection
+How to install all :
+- Copy this [link](https://raw.githubusercontent.com/rektpartyaftermath/Legado-booksource-collection/main/allbookSource.json)
+- Open legado
+- Find import online
+- Paste then import
+
+Install individualy :
+- Find the booksource u want [here](List.md)
+- Copy the link
+- Find import online
+- Paste then import
--- update.sh
+++ update.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+# Get list of all folder
+bookSourceList=$(ls ./Booksource)
+rm List.md
+for i in $bookSourceList
+do
+	echo "[$i](https://raw.githubusercontent.com/rektpartyaftermath/Legado-booksource-collection/main/$i/bookSource.json)" >> List.md
+done
+# make a link to each of them but change it to
+# https://raw.githubusercontent.com/rektpartyaftermath/Legado-Book-Source-id-eng/main/bookSource.json
+# Merge all Booksource
 jq '.[0]' ./Booksource/*/*json | jq -s '.' > AllBooksource.json
